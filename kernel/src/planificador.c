f9b98896e3984681d0966690cfae60b60a853154
Santibouvet <santinobouvet@gmail.com>
2024-09-08 18:50:07 +0000

    comienzo de planteo del planificador Kernel

---
+++ kernel/src/planificador.c
@@ -0,0 +1,31 @@
+/*
+Tipos de planificacion:
+FIFO
+Prioridades
+Colas multinivel
+*/
+
+/*
+Instrucciones o sycalls a utilizar
+
+procesos:
+PROCESS_CREATE(nombre del archivo pseudocodigo que ejecutara el proceso, tamaño del proceso, prioridad del hilo)
+PROCESS_ EXIT()
+
+threads:
+THREAD_CREATE(nombre del archivo pseudocodigo que debera ejecutar el hilo, prioridad)
+THREAD_JOIN(TID)
+THREAD_CANCEL(TID)
+THREAD_EXIT()
+
+mutex:
+MUTEX_CREATE()
+MUTEX_LOCK()
+MUTEX_UNLOCK()
+
+memoria:
+DUMP_MEMORY()
+
+entrada y salida:
+IO(cantidad de milisegundos que el hilo va a permanecer haciendo la operación de entrada/salida)
+*/
